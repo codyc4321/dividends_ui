661ac4a4466edddf9a55d7cc6aaf82001b220a62
Cody <cchilder@mail.usf.edu>
2022-07-28 16:23:01 -0400

    added current price

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 class App extends React.Component {
 
   state = {
+    current_price: null,
     current_yield: null,
     dividend_change_1_year: null,
     dividend_change_3_year: null,
@@ -18,51 +19,37 @@ class App extends React.Component {
   }
 
   onSearchSubmit = async (term) => {
-    const yield_url = 'http://localhost:8000/dividends/current_yield/' + term
-    console.log(yield_url)
-    const response = await axios.get(yield_url, {
-      // params: {query: term},
-      headers: {  }
-    });
+    const price_url = 'http://localhost:8000/dividends/current_price/' + term
+    const price_response = await axios.get(price_url, {});
+    this.setState({current_price: price_response.data['current_price']});
 
-    console.log(response);
-    console.log("response data:")
-    console.log(response.data);
-    this.setState({current_yield: response.data['current_yield']});
+    const yield_url = 'http://localhost:8000/dividends/current_yield/' + term
+    const yield_response = await axios.get(yield_url, {});
+    this.setState({current_yield: yield_response.data['current_yield']});
 
     // http://localhost:8000/dividends/dividend_yield_change/psec/10
     const one_year_change_url = 'http://localhost:8000/dividends/dividend_yield_change/' + term + '/1';
-    const response_1_year_change = await axios.get(one_year_change_url, {
-      headers: {  }
-    });
+    const response_1_year_change = await axios.get(one_year_change_url, {});
     this.setState({dividend_change_1_year: response_1_year_change.data['change']});
 
 
     const three_year_change_url = 'http://localhost:8000/dividends/dividend_yield_change/' + term + '/3';
-    const response_3_year_change = await axios.get(three_year_change_url, {
-      headers: {  }
-    });
+    const response_3_year_change = await axios.get(three_year_change_url, {});
     this.setState({dividend_change_3_year: response_3_year_change.data['change']});
 
 
     const five_year_change_url = 'http://localhost:8000/dividends/dividend_yield_change/' + term + '/5';
-    const response_5_year_change = await axios.get(five_year_change_url, {
-      headers: {  }
-    });
+    const response_5_year_change = await axios.get(five_year_change_url, {});
     this.setState({dividend_change_5_year: response_5_year_change.data['change']});
 
 
     const ten_year_change_url = 'http://localhost:8000/dividends/dividend_yield_change/' + term + '/10';
-    const response_10_year_change = await axios.get(ten_year_change_url, {
-      headers: {  }
-    });
+    const response_10_year_change = await axios.get(ten_year_change_url, {});
     this.setState({dividend_change_10_year: response_10_year_change.data['change']});
 
 
     const all_dividends_url = 'http://localhost:8000/dividends/all_dividends/' + term + '/3';
-    const response_all_dividends = await axios.get(all_dividends_url, {
-      headers: {  }
-    });
+    const response_all_dividends = await axios.get(all_dividends_url, {});
     this.setState({all_dividends: response_all_dividends.data.reverse()});
   }
 
@@ -70,13 +57,12 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <h3>The current_yield for this stock is: {this.state.current_yield}%</h3>
-
+        <h3>The current price for this stock is: {this.state.current_price}%</h3>
+        <h3>The current yield for this stock is: {this.state.current_yield}%</h3>
         <h3>The 1 year dividend change for this stock is: {this.state.dividend_change_1_year}%</h3>
         <h3>The 3 year dividend change for this stock is: {this.state.dividend_change_3_year}%</h3>
         <h3>The 5 year dividend change for this stock is: {this.state.dividend_change_5_year}%</h3>
         <h3>The 10 year dividend change for this stock is: {this.state.dividend_change_10_year}%</h3>
-
         <h3>The dividends for the last 3 years:</h3>
         <br/>
         <DividendResults all_dividends={this.state.all_dividends} />
