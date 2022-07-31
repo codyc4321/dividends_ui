9a10620f9b15386e98fc6241cf0abae2021b092e
Cody <cchilder@mail.usf.edu>
2022-07-31 15:35:59 -0400

    turned dividends results into a nice table

--- src/components/DividendResults.js
+++ src/components/DividendResults.js
@@ -1,14 +1,37 @@
 import React from 'react';
 
+import './DividendResults.css';
+
 
 const DividendResults = props => {
+  //
+  // const dividends = props.all_dividends.map((dividends_object) => {
+  //   return <h4 key={dividends_object.date}>{dividends_object.date} --- {dividends_object.amount}</h4>
+  // });
 
-  const dividends = props.all_dividends.map((dividends_object) => {
-    return <h4 key={dividends_object.date}>{dividends_object.date} --- {dividends_object.amount}</h4>
+  const dividends_rows = props.all_dividends.map((dividends_object) => {
+    return (
+      <tr key={dividends_object.date}>
+        <td>{dividends_object.date}</td>
+        <td>{dividends_object.amount}</td>
+      </tr>
+    )
   });
 
   return (
-    <div>{dividends}</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dividends_rows}
+        </tbody>
+      </table>
+    </div>
   );
 
 };
