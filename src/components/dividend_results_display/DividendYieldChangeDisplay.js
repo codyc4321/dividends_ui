a0fb576af9b75a5faff13453911cee293f90d1f0
Cody <cchilder@mail.usf.edu>
2022-08-02 19:14:44 -0400

    made DividendYieldChangeDisplay functional

--- src/components/dividend_results_display/DividendYieldChangeDisplay.js
+++ src/components/dividend_results_display/DividendYieldChangeDisplay.js
@@ -1,40 +1,68 @@
 import React from 'react';
 
 
-class DividendYieldChangeDisplay extends React.Component {
-
-  componentDidMount() {
-    console.log("componentDidMount in dividend yield display");
-    console.log(this.props)
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>The dividend yield change over time:</h3>
-        <table>
-          <tbody>
-            <tr>
-              <td>1 year</td>
-              <td>{this.props.dividend_change_1_year}%</td>
-            </tr>
-            <tr>
-              <td>3 year</td>
-              <td>{this.props.dividend_change_3_year}%</td>
-            </tr>
-            <tr>
-              <td>5 year</td>
-              <td>{this.props.dividend_change_5_year}%</td>
-            </tr>
-            <tr>
-              <td>10 year</td>
-              <td>{this.props.dividend_change_10_year}%</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+const DividendYieldChangeDisplay = (props) => {
+  return (
+    <div>
+      <h3>The dividend yield change over time:</h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>1 year</td>
+            <td>{this.props.dividend_change_1_year}%</td>
+          </tr>
+          <tr>
+            <td>3 year</td>
+            <td>{this.props.dividend_change_3_year}%</td>
+          </tr>
+          <tr>
+            <td>5 year</td>
+            <td>{this.props.dividend_change_5_year}%</td>
+          </tr>
+          <tr>
+            <td>10 year</td>
+            <td>{this.props.dividend_change_10_year}%</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
+//
+// class DividendYieldChangeDisplay extends React.Component {
+//
+//   componentDidMount() {
+//     console.log("componentDidMount in dividend yield display");
+//     console.log(this.props)
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h3>The dividend yield change over time:</h3>
+//         <table>
+//           <tbody>
+//             <tr>
+//               <td>1 year</td>
+//               <td>{this.props.dividend_change_1_year}%</td>
+//             </tr>
+//             <tr>
+//               <td>3 year</td>
+//               <td>{this.props.dividend_change_3_year}%</td>
+//             </tr>
+//             <tr>
+//               <td>5 year</td>
+//               <td>{this.props.dividend_change_5_year}%</td>
+//             </tr>
+//             <tr>
+//               <td>10 year</td>
+//               <td>{this.props.dividend_change_10_year}%</td>
+//             </tr>
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// };
 
 export default DividendYieldChangeDisplay;
