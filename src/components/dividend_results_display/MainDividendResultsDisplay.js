c30d174618a0a64a70bbfd077114da4f45051618
Cody <cchilder@mail.usf.edu>
2022-08-02 19:21:14 -0400

    made MainDividendResultsDisplay functional

--- src/components/dividend_results_display/MainDividendResultsDisplay.js
+++ src/components/dividend_results_display/MainDividendResultsDisplay.js
@@ -1,32 +1,30 @@
 import React from 'react';
 
 
-class MainDividendResultsDisplay extends React.Component {
+const MainDividendResultsDisplay = (props) => {
+  return(
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Price</td>
+            <td>${props.current_price}</td>
+          </tr>
 
-  render() {
-    return(
-      <div>
-        <table>
-          <tbody>
-            <tr>
-              <td>Price</td>
-              <td>${this.props.current_price}</td>
-            </tr>
+          <tr>
+            <td>Yearly dividends</td>
+            <td>${props.recent_dividend_rate}</td>
+          </tr>
 
-            <tr>
-              <td>Yearly dividends</td>
-              <td>${this.props.recent_dividend_rate}</td>
-            </tr>
+          <tr>
+            <td>Yield</td>
+            <td>{props.current_yield}%</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-            <tr>
-              <td>Yield</td>
-              <td>{this.props.current_yield}%</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-};
 
 export default MainDividendResultsDisplay;
