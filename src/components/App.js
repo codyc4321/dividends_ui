e02369e2a396d24b36134caf248609ade61a8312
Cody Childers <cchilder@mail.usf.edu>
2022-08-04 11:55:19 -0400

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -36,18 +36,18 @@ class App extends React.Component {
   runStockInfoSearch = async (term) => {
     console.log("running search")
     // clear old data
-    // this.setState({
-    //   loading: true,
-    //
-    //   current_price: '',
-    //   recent_dividend_rate: '',
-    //   current_yield: '',
-    //   dividend_change_1_year: '',
-    //   dividend_change_3_year: '',
-    //   dividend_change_5_year: '',
-    //   dividend_change_10_year: '',
-    //   all_dividends: [],
-    // });
+    this.setState({
+      loading: true,
+
+      current_price: '',
+      recent_dividend_rate: '',
+      current_yield: '',
+      dividend_change_1_year: '',
+      dividend_change_3_year: '',
+      dividend_change_5_year: '',
+      dividend_change_10_year: '',
+      all_dividends: [],
+    });
 
     const HOST = 'localhost';
     const base_url = 'http://' + HOST + ':8000'
@@ -64,44 +64,17 @@ class App extends React.Component {
           'recent_dividend_rate'
         ]
         RESPONSE_KEYS.map((key) => {
-          // The old way I did it that worked, but created a messy state with 7 attributes
-          // this.setState({dividends_data[key]: response.data[key]});
-
-          // First attempt based on SO answer
-          // this.setState(previousState => {
-          //   let data = Object.assign({}, previousState.data);
-          //   console.log("data: ", data)
-          //   data[key] = response.data[key]
-          //   return {data}
-          // });
-
-          // this.setState(prevState => ({
-          //   data: {
-          //     ...prevState.data,
-          //     [key]: response.data[key],
-          //   }
-          // }));
-
-          // Another attempt from the answer below the accepted answer
           this.updateStateData(key, response.data[key]);
-          // const data = this.state.dividends_data;
-          // data[key] = response.data[key];
-          // this.setState({data});
         })
 
-        const data = this.state.dividends_data;
-        data.all_dividends = response.data['all_dividends'].reverse();
-        this.setState({data});
+
+        this.updateStateData('all_dividends', response.data['all_dividends'].reverse());
 
 
         const YEARS_CHANGE = [1, 3, 5, 10];
         YEARS_CHANGE.map((year) => {
           const key = 'dividend_change_' + year.toString() + '_year';
-          const data = this.state.dividends_data;
-          data[key] = response.data[key];
-          this.setState({data});
-
-          // this.setState({[key]: response.data[key]})
+          this.updateStateData(key, response.data[key]);
         });
 
         this.setState({loading: false})
