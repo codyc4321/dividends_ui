aa2d7abe928493653b75e0ae63ca568578e0bc92
Cody <cchilder@mail.usf.edu>
2022-08-04 12:20:49 -0400

    using env var for host

--- src/components/App.js
+++ src/components/App.js
@@ -49,7 +49,7 @@ class App extends React.Component {
       all_dividends: [],
     });
 
-    const HOST = 'localhost';
+    const HOST = process.env.REACT_APP_HOSTNAME
     const base_url = 'http://' + HOST + ':8000'
     const dividends_api_url = base_url + '/dividends/' + term
 
