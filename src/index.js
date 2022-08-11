0d1e7d55515870ca5fe68c65702381ca844ccad1
Cody <cchilder@mail.usf.edu>
2022-08-11 03:07:40 -0400

    got sign in with google working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+// import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
 
-createRoot(document.getElementById('root')).render(<App/>)
+const store = createStore(reducers)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
