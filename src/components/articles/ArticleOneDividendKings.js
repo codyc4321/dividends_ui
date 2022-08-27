e0dca40d410346410ebe2fb6fd5ec92ae134f18b
Cody <cchilder@mail.usf.edu>
2022-08-27 05:28:12 -0400

    added article 1

--- src/components/articles/ArticleOneDividendKings.js
+++ src/components/articles/ArticleOneDividendKings.js
@@ -15,6 +15,45 @@ const ArticleOneDividendKings = () => {
         <li>Must maintain liquidity of a daily stock trade value of $5 million for at least 3 months before entering the list</li>
         <li>Must maintain a total market cap of $3 billion (float-adjusted)</li>
       </ol>
+      <p>Some well-known dividend aristocrats are Aflac (AFL), IBM, Walgreens (WBA), Chlorox (CLX), McDonald's (MCD), Pepsi (PEP), and Walmart (WMT).</p>
+      <p>Examples of well-known dividend kings include Coca-Cola (KO), Johnson & Johnson (JNJ),
+      3M (MMM, they make most of the tape you use), and Target superstore (TGT).</p>
+      <p>Although the extra criteria for aristocrats is more specific, some dividend kings are also aristocrats.</p>
+      <p>Because 50 years of raised dividends is much longer than 25 years, there is usually less kings than aristocrats. Most recently, there are 65 aristocrats and 45 kings.</p>
+      <p>Keep in mind, that a consistently raising dividend does not guarentee a high dividend yield.
+      However, a stock with a raising dividend held longterm over 20-30 years may start out as a 2%
+      dividend on the day you bought the stock, but decades later could become 10% or higher on your
+      initial investment. Consider a stock you buy today for $100, with a 2% dividend. If the yearly
+      dividend rate increases to $10 per year 20 year from now, you will be earning 10% on your initial investment, plus
+      all the capital gains the stock experienced.  While a raising dividend doesn't guarentee capital gains, as
+      dividends rise, the stock price will oftentimes rise with it. As an example, use our search feature an type in 'HD'.
+      Set the dividend search to 20 years, and see how much Home Depot's dividend has increased longterm.</p>
+      <h4>Pros and Cons of buying aristocrats</h4>
+      <p>Buying dividend aristocrats may not be an ideal strategy for every investor's goals. Some dividend aristocrats' total returns have recently underperformed the S&P 500.</p>
+      <h5>Pros:</h5>
+      <ul>
+        <li>Increasing dividends provide stable income for people relying on passive income (disabled, retired, etc)</li>
+        <li>Longterm rising dividends signal strong financial strength of a company, and proves more safety as an investment</li>
+        <li>Over a long time span, the dividend yield you will earn on your investment increases</li>
+        <li>Dividend aristocrats and kings tend to be resilient in recessions, declining in value less than newer and less stable companies</li>
+      </ul>
+      <h5>Cons:</h5>
+      <ul>
+        <li>Dividends paid out leave less money for reinvestment and company growth</li>
+        <li>Due to con above, high dividend paying stocks usually produce less capital gains than growth stocks</li>
+        <li>Dividends are taxable as income, and can cause hardship to your yearly tax bill</li>
+      </ul>
+      <p>For retirees especially, the lower volatility and safe income of these stocks can be a large advantage in their portfolio.
+      When you no longer work for income, large swings in your portfolio value can cause losses if you need to sell stocks to raise cash in the short term.</p>
+      <h4>Dividend aristocrats vs. the market?</h4>
+      <p>While this answer depends on timing, as of 2021 and over the last decade, the aristocrats as a whole have
+      returned 14.3% compared to 14.2% for the entire S&P 500. Adjusted for risk, however, the lower volatility of the aristocrats is an advantage.</p>
+      <h4>What's the best way to invest in dividend aristocrats?</h4>
+      <p>SPYD and NOBL are two popular ETFs with high dividend yields that sample the dividend aristocrats.
+      As of 2022 there is no ETF dedicated entirely to the dividend kings.</p>
+      <p></p>
+      <p></p>
+      <p></p>
       <p></p>
       <p></p>
       <p></p>
