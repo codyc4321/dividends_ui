31834c375762ab9ea036e7eb8fa6d1caed08acf2
Cody <cchilder@mail.usf.edu>
2022-08-30 05:06:26 -0400

    finished investing-basics

---
+++ src/components/articles/Quotes.js
@@ -0,0 +1,6 @@
+
+
+
+// The goal of the non-professional should not be to pick winners — neither he nor his 'helpers' can do that — but should rather be to own a cross-section of businesses that in aggregate are bound to do well.
+// Warren Buffett
+// https://www.wealthsimple.com/en-ca/learn/investing-basics#basics_of_investing_in_stocks
--- src/components/articles/basics/ArticleBasicsOfInvesting.js
+++ src/components/articles/basics/ArticleBasicsOfInvesting.js
@@ -1,4 +1,5 @@
 import authors from '../authors';
+import TickerLink from '../../shared/TickerLink';
 
 
 const body = (
@@ -38,38 +39,72 @@ const body = (
     <h4>Common Stock</h4>
     <p>Common stock (usually just referred to as stock since it is much more common than preferred stock) is the most risky yet most rewarding asset class.
     While bonds and preferred stocks can appreciate in value (cash depreciates) common stock has unlimited upside and can allow massive returns
-    as small companies can grow very large. One of the most popular stock funds, VTI, has returned 10% on average over the last 30 years.
+    as small companies can grow very large. One of the most popular stock funds, <TickerLink ticker="VTI"/>, which samples the entire stock market, has returned 10% on average over the last 30 years.
     In fact, according to <a href="https://www.nerdwallet.com/article/investing/average-stock-market-return" target="_blank">Nerd Wallet</a>, the
     average return of the stock market is 10%. However, the S&P 500 has returned a &nbsp;
     <a href="https://www.businessinsider.com/personal-finance/average-stock-market-return" target="_blank">massive 14.7%</a> over the last 10 years.&nbsp;
     Another big advantage of common stock is that it confers voting rights to its owners on a per-share basis. Common stock is riskier because any dividends
     offered are last in line to be paid after bonds and preferred stock, and in a bankruptcy common stockholders are also last to be paid after bonds and preferreds.</p>
 
+    <h4>Types of Stocks</h4>
+    <p>There are 4 main types of investments traded on the stock market:</p>
+    <ul>
+      <li>Individual (Common) Stocks</li>
+      <li>Exchange-traded Funds (ETFs)</li>
+      <li>Real Estate Investment Trusts (REITs)</li>
+      <li>Preferred Stocks</li>
+    </ul>
+    <p>Individual Stocks are the riskiest investment in the stock market, because when a company goes bankrupt the common stock usually goes to $0 with no compensation.</p>
+    <p>ETFs are less risky than stocks, because an ETF is a collection of a few dozen up to a few hundred stocks. The odds of an ETF going to $0 is slim to none,
+    although ETFs can still decrease in value severely. Investing in dozens of stocks at one time by buying a single ETF follows a main principle of good investing
+    called "diversification". Continue reading this article for an explanation on why to diversify.</p>
+    <p>Real estate investment trusts are companies (which are more like funds) that invest the majority of their capital into real estate.
+    The 2 main types of REITs are equity REITs, which are funds that own and operate real estate for income, and mortgage REITs, which invest in mortgages of
+    various real estate and receive interest payments from borrowers. Real estate trusts are considered an alternative investment, compared to "traditional"
+    stocks and bonds.&nbsp;
+    REITs and real estate are loved by many investors due to helping them diversify and its low price correlation with stocks and bonds. It is very possible for the
+    stock market and bonds to be down while real estate prices are sky-high.</p>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+
+
     <h4>How to Invest</h4>
-    The most common way to start investing is to open an online brokerage account at a national brokerage like&nbsp;
+    <p>The most common way to start investing is to open an online brokerage account at a national brokerage like&nbsp;
     <a href="https://www.schwab.com/open-an-account" target="_blank">Charles Schwab</a>,&nbsp;
     <a href="https://www.fidelity.com/go/starter-pack" target="_blank">Fidelity</a>, or&nbsp;
     <a href="https://www.tdameritrade.com/tools-and-platforms/trader-offering.html" target="_blank">TD Ameritrade</a>.&nbsp;
-    I recommend Charles Schwab because the customer service is amazing, even if you don't have much money in your account. 
+    I recommend Charles Schwab because the customer service is amazing, even if you don't have much money in your account.&nbsp;
+    Schwab also has local branches they invite clients into for a free retirement consultation. Online brokerages almost never charge fees for stock/ETF/REIT trades.</p>
+    <p>Another option is to have a professional brokerage place trades for you and buy your stocks. Full-service brokerages may charge more fees but for
+    some investors who don't want the burden of researching and picking every single stock having a helping hand oversee their investment is helpful.</p>
 
+    <h4>Diversification</h4>
+    <p>Diversification is always summarized as the old saying "don't put all your eggs in one basket". What this means is that only having one type of one of
+    something can be risky if that one thing doesn't work out. While we mentioned that investing has risks, the reason so many people do it successfully
+    is the concept of diversifying your investment portfolio.</p>
+    <p>Diversifying a portfolio works because if you own portions of hundreds of stocks or bonds, it is very unlikely that hundreds of investments will all go down
+    together over a long timespan. If you put all your money into 4 stocks equally, and one stock went bankrupt, you would lose a massive 25% of your portfolio overnight.</p>
+    <p>Diversification usually includes investing in several asset classes, most notably stocks, bonds, real estate, and commodities. However, even if you only
+    owned stocks, you can diversify an all stock portfolio by buying stocks in different countries, stocks in different industries, stocks in different stages
+    (startup, growing, mature), and stocks of different company size (small, medium, large).</p>
+    <p>Diversifying is extremely important in long-term investing because it dramatically lowers the risk you are taking. It not only lowers your risk of long-term losses,
+    it also lowers your investments' volatility, or short-term losses. While diversifying is an important goal, you should only invest in quality companies.</p>
 
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
+    <h4>How to Diversify</h4>
+    <p>Many experts recommend owning 20-30 stocks of different industries. However, some researchers &nbsp;
+    <a href="https://time.com/nextadvisor/investing/how-many-stocks-should-you-own-in-portfolio" target="_blank">recommend a minimum of 60.</a></p>
+    <p>This is because different economic conditions affect stocks in different industries in different ways. For example, in a recession,
+    consumer discretionary companies (nice to have items like luxury cars) decline in value as most people have less income. However, industries like
+    consumer staples (toothpaste, shampoo), grocery stores, dollar stores, and cosmetics companies usually do fine in recessions.</p>
+    <p>When the economy is strong and demand for goods and services is high, luxury clothing and energy companies can thrive.</p>
+    <p>With that said, the easiest way to diversify amongst stocks is to buy several different ETFs, or even index funds.
+    While we discuess ETFs holding dozens of different stocks, index funds are a type of ETF that sample a specific sector of the economy, such as
+    an insurance company index fund, S&P 500 index fund, or oil index fund. Index funds are passively managed and have lower fees than actively managed funds.
+    The leader in ETFs and index funds is <a href="https://investor.vanguard.com/investment-products/list/etfs" target="_blank">Vanguard</a>,
+    and their ETF fees are usually the lowest in the entire industry. Compare Vanguard's 0.2% to 0.04% fees to most ETF's 1% fees and the savings you
+    get by investing in Vanguard can add up to tens of thousands of dollars over 30 years.</p>
     <p></p>
     <p></p>
     <p></p>
@@ -100,6 +135,26 @@ const ArticleBasicsOfInvesting = {
     {
       title: "“A Guide to Investing in Preferred Stocks.” World of Dividends by Simply Safe Dividends, 16 Aug. 2022",
       url: "https://www.simplysafedividends.com/world-of-dividends/posts/2094-a-guide-to-investing-in-preferred-stocks"
+    },
+    {
+      title: "“Learn the Basics of REITS & Reit Investing.” REIT Basics | Learn the Basics of REITS & REIT Investing | Nareit",
+      url: "https://www.reit.com/what-reit/reit-basics"
+    },
+    {
+      title: "DiLallo, Matthew. “3 Tips for a Diversified Portfolio.” The Motley Fool",
+      url: "https://www.fool.com/investing/how-to-invest/portfolio-diversification/"
+    },
+    {
+      title: "“How Many Stocks Should You Have in Your Portfolio? | Nextadvisor with Time.” Time, Time, 2 May 2022",
+      url: "https://time.com/nextadvisor/investing/how-many-stocks-should-you-own-in-portfolio"
+    },
+    {
+      title: "Rosenberg, Eric. “5 Recession Resistant Industries.” Investopedia, Investopedia, 13 July 2022",
+      url: "https://www.investopedia.com/articles/investing/100115/5-recession-resistant-industries.asp"
+    },
+    {
+      title: "Fernando, Jason. “Index Fund Definition.” Investopedia, Investopedia, 28 July 2022",
+      url: "https://www.investopedia.com/terms/i/indexfund.asp"
     },
   ]
 }
