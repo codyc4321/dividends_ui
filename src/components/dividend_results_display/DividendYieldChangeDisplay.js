51143bb48fe23c631d04124fcb0eeb0ef93100e6
Cody <cchilder@mail.usf.edu>
2022-09-01 05:33:31 -0400

    fixed bug where % would show up even if there was no dividend back then

--- src/components/dividend_results_display/DividendYieldChangeDisplay.js
+++ src/components/dividend_results_display/DividendYieldChangeDisplay.js
@@ -12,19 +12,19 @@ const DividendYieldChangeDisplay = (props) => {
         <tbody>
           <tr>
             <td>1 year</td>
-            <td>{props.dividend_change_1_year}%</td>
+            <td>{props.dividend_change_1_year}{isNaN(props.dividend_change_1_year) ? '' : '%'}</td>
           </tr>
           <tr>
             <td>3 year</td>
-            <td>{props.dividend_change_3_year}%</td>
+            <td>{props.dividend_change_3_year}{isNaN(props.dividend_change_3_year) ? '' : '%'}</td>
           </tr>
           <tr>
             <td>5 year</td>
-            <td>{props.dividend_change_5_year}%</td>
+            <td>{props.dividend_change_5_year}{isNaN(props.dividend_change_5_year) ? '' : '%'}</td>
           </tr>
           <tr>
             <td>10 year</td>
-            <td>{props.dividend_change_10_year}%</td>
+            <td>{props.dividend_change_10_year}{isNaN(props.dividend_change_10_year) ? '' : '%'}</td>
           </tr>
         </tbody>
       </table>
